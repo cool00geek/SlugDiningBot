911f5d0140e4d0e9d37c99efa54fc81b0626f003
Vinay V <cool00geek@yahoo.com>
2019-01-26 21:38:58 -0800

    Add option to specify meal

--- UCSCDining/TelegramBot.py
+++ UCSCDining/TelegramBot.py
@@ -59,7 +59,7 @@ def search(bot, update):
     for x in msg_list:
         msg_str += x + " "
     msg_str = msg_str[:-1]
-    bot.send_message(chat_id=update.message.chat_id, text=DiningBot.search(msg_str))
+    bot.send_message(chat_id=update.message.chat_id, text=DiningBot.search(msg_str, meal=meal))
     
 
 def unknown(bot, update):
